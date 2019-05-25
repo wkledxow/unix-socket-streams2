c5bd9a23daa4bebd9ad234c35ec230da27e400ea
Kris Reeves <kris@pressbuttonllc.com>
2019-05-25 15:24:59 +0000

    Update dependencies

--- test/dgram.js
+++ test/dgram.js
@@ -1,18 +1,27 @@
 'use strict';
 
+require('should');
+
 var Promise = require('bluebird');
 
 var unix = require('unix-dgram'),
     fs = require('fs');
-    
+
 var UnixSocket = require('../index');
 
 Promise.promisifyAll(fs, { suffix: '$' });
 Promise.promisifyAll(UnixSocket.prototype, { suffix: '$' });
 
-require('should-eventually');
-
 var TEST_SOCKET = '/tmp/socket-test';
+
+function defer() {
+    var deferred = { };
+    deferred.promise = new Promise(function (resolve, reject) {
+        deferred.resolve = resolve;
+        deferred.reject = reject;
+    });
+    return deferred;
+}
 
 describe('datagram sockets', function () {
     function getServer(onMessage) {
@@ -26,23 +35,23 @@ describe('datagram sockets', function () {
         })
         .disposer(function () {
             server.close()
-            
+
             return fs.unlink$(TEST_SOCKET)
             .catch(function (e) { if (e.cause.code !== 'ENOENT') { throw e; } });
         });
     }
-    
+
     it('should connect, pass a message, and quit gracefully', function () {
         var count = 0;
-        var deferred = Promise.defer();
+        var deferred = defer();
         var server = getServer(function (buf) {
             count++;
             deferred.resolve(buf.toString());
         });
-        
+
         return Promise.using(server, function () {
             var socket = new UnixSocket();
-            
+
             return socket.connect$(TEST_SOCKET, { type: 'dgram' })
             .then(function () {
                 return socket.write$('foo');
@@ -57,14 +66,14 @@ describe('datagram sockets', function () {
             count.should.equal(1);
         });
     });
-    
+
     it('should emit a \'close\' event', function () {
-        var deferred = Promise.defer();
+        var deferred = defer();
         var server = getServer(function () { });
-        
+
         return Promise.using(server, function () {
             var socket = new UnixSocket();
-            
+
             return socket.connect$(TEST_SOCKET, { type: 'dgram' })
             .then(function () {
                 socket.once('close', deferred.resolve.bind(deferred));
@@ -76,18 +85,18 @@ describe('datagram sockets', function () {
 
     it('should support node-unix-datagram\'s congestion control', function () {
         var count = 0;
-        var deferred = Promise.defer(),
-            deferred2 = Promise.defer();
-        
+        var deferred = defer(),
+            deferred2 = defer();
+
         var server = getServer(function (buf) {
             count++;
             if (count === 1) { deferred.resolve(buf.toString()); }
             if (count === 2) { deferred2.resolve(buf.toString()); }
         });
-        
+
         return Promise.using(server, function () {
             var socket = new UnixSocket();
-            
+
             return socket.connect$(TEST_SOCKET, { type: 'dgram' })
             .then(function () {
                 return socket.write$('foo');
@@ -107,18 +116,18 @@ describe('datagram sockets', function () {
             count.should.equal(2);
         });
     });
-    
+
     it('should switch to datagram for datagram sockets opened as tcp', function () {
         var count = 0;
-        var deferred = Promise.defer();
+        var deferred = defer();
         var server = getServer(function (buf) {
             count++;
             deferred.resolve(buf.toString());
         });
-        
+
         return Promise.using(server, function () {
             var socket = new UnixSocket();
-            
+
             return socket.connect$(TEST_SOCKET, { type: 'tcp' })
             .then(function () {
                 return socket.write$('foo');
@@ -133,21 +142,21 @@ describe('datagram sockets', function () {
             count.should.equal(1);
         });
     });
-    
+
     it('should not double-emit \'close\' when switching from tcp to udp', function () {
         var count = 0;
-        var deferred = Promise.defer();
+        var deferred = defer();
         var server = getServer(function (buf) {
             count++;
             deferred.resolve(buf.toString());
         });
         var closed = 0;
-        
+
         return Promise.using(server, function () {
             var socket = new UnixSocket();
-            
+
             socket.on('close', function () { closed++; });
-            
+
             return socket.connect$(TEST_SOCKET, { type: 'tcp' })
             .then(function () {
                 return socket.write$('foo');
--- test/tcp.js
+++ test/tcp.js
@@ -1,26 +1,35 @@
 'use strict';
 
+require('should');
+
 var Promise = require('bluebird');
 
 var net = require('net'),
     fs = require('fs');
-    
+
 var UnixSocket = require('../index');
 
 Promise.promisifyAll(fs, { suffix: '$' });
 Promise.promisifyAll(net.Socket.prototype, { suffix: '$' });
 Promise.promisifyAll(UnixSocket.prototype, { suffix: '$' });
 
-require('should-eventually');
-
 var TEST_SOCKET = '/tmp/socket-test';
+
+function defer() {
+    var deferred = { };
+    deferred.promise = new Promise(function (resolve, reject) {
+        deferred.resolve = resolve;
+        deferred.reject = reject;
+    });
+    return deferred;
+}
 
 describe('tcp sockets', function () {
     var server;
-    
+
     function getServer(onMessage) {
         var server;
-        
+
         return fs.unlink$(TEST_SOCKET)
         .catch(function (e) { if (e.cause.code !== 'ENOENT') { throw e; } })
         .then(function () {
@@ -39,18 +48,18 @@ describe('tcp sockets', function () {
             });
         });
     }
-    
+
     it('should connect, pass a message, and quit gracefully', function () {
         var count = 0;
-        var deferred = Promise.defer();
+        var deferred = defer();
         var server = getServer(function (buf) {
             count++;
             deferred.resolve(buf.toString());
         });
-        
+
         return Promise.using(server, function () {
             var socket = new UnixSocket();
-            
+
             return socket.connect$(TEST_SOCKET, { type: 'tcp' })
             .then(function () {
                 return socket.write$('foo');
@@ -65,14 +74,14 @@ describe('tcp sockets', function () {
             count.should.equal(1);
         });
     });
-    
+
     it('should emit a \'close\' event', function () {
-        var deferred = Promise.defer();
+        var deferred = defer();
         var server = getServer(function () { });
-        
+
         return Promise.using(server, function () {
             var socket = new UnixSocket();
-            
+
             return socket.connect$(TEST_SOCKET, { type: 'dgram' })
             .then(function () {
                 socket.once('close', deferred.resolve.bind(deferred));
@@ -81,18 +90,18 @@ describe('tcp sockets', function () {
             });
         });
     });
-    
+
     it('should switch to tcp for tcp sockets opened as datagram sockets', function () {
         var count = 0;
-        var deferred = Promise.defer();
+        var deferred = defer();
         var server = getServer(function (buf) {
             count++;
             deferred.resolve(buf.toString());
         });
-        
+
         return Promise.using(server, function () {
             var socket = new UnixSocket();
-            
+
             return socket.connect$(TEST_SOCKET, { type: 'dgram' })
             .then(function () {
                 return socket.write$('foo');
@@ -107,6 +116,6 @@ describe('tcp sockets', function () {
             count.should.equal(1);
         });
     });
-    
+
 });
 
